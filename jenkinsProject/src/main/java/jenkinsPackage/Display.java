6f307d6ca5d2040baa40350adbfafd49860230db
Lsn-User <Lsn-User@DESKTOP-GLKVRGN>
2024-03-24 13:45:24 +0530

    first commit

---
+++ jenkinsProject/src/main/java/jenkinsPackage/Display.java
@@ -0,0 +1,13 @@
+package jenkinsPackage;
+
+public class Display {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+			
+		
+
+	}
+
+}
---
+++ jenkinsProject/src/main/java/jenkinsPackage/Show.java
@@ -0,0 +1,11 @@
+package jenkinsPackage;
+
+public class Show {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Good Mornin");
+
+	}
+
+}
